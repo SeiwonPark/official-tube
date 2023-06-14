d4d953fb88b11dfbdfd92356fd739d70b8a90f94
SeiwonPark <psw7347@gmail.com>
2023-06-14 21:42:35 +0900

    feat: add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    background: "./src/workers/background.js",
+    content: "./src/scripts/content.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+};
