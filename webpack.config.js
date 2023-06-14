7416ff68541de570e8ed7af2eb6ee706256ed0ba
SeiwonPark <psw7347@gmail.com>
2023-06-15 00:17:20 +0900

    build: babel to swc

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -17,12 +18,21 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+        use: [
+          {
+            loader: "swc-loader",
+            options: {
+              jsc: {
+                parser: {
+                  syntax: "ecmascript",
+                  jsx: true,
+                  dynamicImport: true,
+                },
+                target: "es2015",
+              },
+            },
           },
-        },
+        ],
       },
       {
         test: /\.css$/i,
@@ -37,5 +47,11 @@ module.exports = {
     new CopyPlugin({
       patterns: [{ from: "public" }],
     }),
+    new NodePolyfillPlugin(),
   ],
+  resolve: {
+    fallback: {
+      fs: false,
+    },
+  },
 };
